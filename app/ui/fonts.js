2f0d82417739de381aa2d6d3aea87caee90adba1
georithTheKoala <georgeskateacids@gmail.com>
2023-12-20 17:22:00 +0700

    first commit

---
+++ app/ui/fonts.js
@@ -0,0 +1,9 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+// adding a font
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
