a82b53ee30d38c81772f90069efc4508415ebf51
georithTheKoala <georgeskateacids@gmail.com>
2023-12-23 20:44:53 +0700

    Adding search and pagination

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -25,10 +25,6 @@ export default async function Page() {
             Dashboard
           </h1>
           <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-            {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-            <Card title="Pending" value={totalPendingInvoices} type="pending" />
-            <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-            <Card title="Total Customers" value={numberOfCustomers} type="customers" /> */}
             <Suspense fallback={<CardsSkeleton />}>
               <CardWrapper />
             </Suspense>
