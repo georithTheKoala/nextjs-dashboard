7b9431a024583ff36e02d00d70551b25acbfeb26
georithTheKoala <georgeskateacids@gmail.com>
2023-12-29 13:07:49 +0700

    adding Revalidate and Redirect

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -24,6 +24,7 @@ export type Invoice = {
   // In TypeScript, this is called a string union type.
   // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
   status: 'pending' | 'paid';
+  data: string;
 };
 
 export type Revenue = {
